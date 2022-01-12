7ca98d0ab1e56deed2a651cc4866cbad5956ebb4
harishankar15 <harishankar151197@gmail.com>
2022-01-12 23:16:18 +0530

    Add Stock changes

--- reckons/reckon/agencyuser/models.py
+++ reckons/reckon/agencyuser/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+#------------------------------------- ADD PRODUCT -------------------------------------------------
 class Productadd(models.Model):
     brandname = models.CharField(max_length= 20)
     hsncode = models.IntegerField(default=0)
@@ -9,10 +10,9 @@ class Productadd(models.Model):
     productprice = models.IntegerField(default=0)
     actualprice = models.IntegerField(default=0)
 
+#------------------------------------- ADD STOCK ---------------------------------------------------
 class Stockadd(models.Model):
-    brandname = models.ForeignKey(Productadd,models.DO_NOTHING,related_name='sto_brandname',db_column='agencyuser_brandname')
-    productname = models.ForeignKey(Productadd,models.DO_NOTHING,related_name='sto_productname',db_column='agencyuser_productname')
-    # brandname = models.CharField(max_length= 20)
-    # productname = models.CharField(max_length=50)
+    brandname = models.CharField(max_length= 20)
+    productname = models.CharField(max_length=50)
     availbags = models.IntegerField(default=0)
     availpieces = models.IntegerField(default=0)
--- reckons/reckon/agencyuser/views.py
+++ reckons/reckon/agencyuser/views.py
@@ -1,7 +1,29 @@
+#------------------------------------------------------------------------------------------------#
+#  PROJECT NAME: RECKONS                                                                         #
+#  DEVELOPED BY: MAGZ TECHNOLOGIES                                                               #
+#  FUNCTION    : BILLING PURPOSE                                                                 #
+#  DATE & TIME :                                                                                 #
+#------------------------------------------------------------------------------------------------#
+
 from django.shortcuts import redirect, render
 from .models import Productadd, Stockadd
-# Create your views here.
 
+#------------------------------------- ADD PRODUCT -------------------------------------------------
+def addproduct(request):
+    if request.method == "POST":
+        bdname = request.POST["brandname"]
+        hsn = request.POST["hsncode"]
+        prodname = request.POST["productname"]
+        prodprice = request.POST["productprice"]
+        actprice = request.POST["actualprice"]
+        proadd = Productadd.objects.create(brandname = bdname, hsncode = hsn, productname = prodname, productprice = prodprice, actualprice = actprice)
+        proadd.save()
+        print("product is added successfully..")
+        return redirect('addproduct')
+    else:
+        return render(request,'addproduct.html')
+
+#------------------------------------- ADD STOCK ---------------------------------------------------
 def addstock(request):
     if request.method == "POST":
         bname = request.POST["bndname"]
@@ -17,28 +39,17 @@ def addstock(request):
         proadd = Productadd.objects.all()
         return render(request,'addstock.html', {"bra": proadd})
 
+#------------------------------------- ADD BILL ----------------------------------------------------
 def addbill(request):
     
     return render(request,'addbill.html')
 
+#------------------------------------- ADD USER ----------------------------------------------------
 def adduser(request):
     
     return render(request,'adduser.html')
 
-def addproduct(request):
-    if request.method == "POST":
-        bdname = request.POST["brandname"]
-        hsn = request.POST["hsncode"]
-        prodname = request.POST["productname"]
-        prodprice = request.POST["productprice"]
-        actprice = request.POST["actualprice"]
-        proadd = Productadd.objects.create(brandname = bdname, hsncode = hsn, productname = prodname, productprice = prodprice, actualprice = actprice)
-        proadd.save()
-        print("product is added successfully..")
-        return redirect('addproduct')
-    else:
-        return render(request,'addproduct.html')
-
+#------------------------------------- ADD OUTLET ---------------------------------------------------
 def addoutlet(request):
     
     return render(request,'addoutlet.html')    
