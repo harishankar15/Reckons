7f3b82b4d4f209fc2d0937cc10cf4d0ab0c705e0
HARI SHANKAR <58343223+harishankar15@users.noreply.github.com>
2022-01-12 16:06:42 +0530

    Delete addproduct directory

--- addproduct/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import addproductModel
-
-class addproductForm(forms.ModelForm):
-    class Meta():
-        model = addproductModel
-        fields = '__all__'
-
