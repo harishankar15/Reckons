ebb74548cff876b3c841213d1b251ee74d4e2ddb
HARI SHANKAR <58343223+harishankar15@users.noreply.github.com>
2022-01-12 16:06:59 +0530

    Delete addstock directory

--- addstock/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AddstockConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'addstock'
--- addstock/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import addstockModel
-
-class addstockForm(forms.ModelForm):
-    class Meta():
-        model = addstockModel
-        fields = '__all__'
-
